865498de38e377c60879c8d52266097240a493e6
Amam-123 <abuhammam04@students.unnes.ac.id>
2025-06-16 00:09:43 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -2,21 +2,22 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-# Konfigurasi halaman
-st.set_page_config(page_title="Stroke Prediction", layout="centered")
-
-# Judul halaman
-st.title("Stroke Prediction Dashboard")
-
-# Load model
+st.title("Stroke Prediction App")
 model = joblib.load("model.pkl")
 
-# Input user
+# INPUT USER
+gender = st.selectbox("Gender", ['Male', 'Female'])
 age = st.number_input("Age", min_value=0)
+hypertension = st.selectbox("Hypertension", [0, 1])
+heart_disease = st.selectbox("Heart Disease", [0, 1])
 avg_glucose = st.number_input("Average Glucose Level", min_value=0.0)
 bmi = st.number_input("BMI", min_value=0.0)
 
+# Ubah input categorical ke bentuk sesuai model
+gender = 1 if gender == 'Male' else 0
+
 # Prediksi
 if st.button("Predict"):
-    prediction = model.predict([[age, avg_glucose, bmi]])
+    input_data = [[gender, age, hypertension, heart_disease, avg_glucose, bmi]]
+    prediction = model.predict(input_data)
     st.success(f"Prediction: {'STROKE' if prediction[0]==1 else 'NO STROKE'}")

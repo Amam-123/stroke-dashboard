9268bd2d1a68829da6973fd9988ac70c03404068
Amam-123 <abuhammam04@students.unnes.ac.id>
2025-06-16 00:27:16 +0700

    Create 3_Training.py

---
+++ pages/3_Training.py
@@ -0,0 +1,20 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
+st.title("3. Pelatihan Model")
+
+df = pd.read_csv("processed_stroke.csv")  # setelah preprocessing
+X = df.drop("stroke", axis=1)
+y = df["stroke"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+acc = accuracy_score(y_test, model.predict(X_test))
+st.write("Akurasi Model:", acc)
+
+joblib.dump(model, "model.pkl")
